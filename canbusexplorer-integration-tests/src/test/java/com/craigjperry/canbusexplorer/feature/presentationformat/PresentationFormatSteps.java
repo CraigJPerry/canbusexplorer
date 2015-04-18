d1f2b9d67c7a6ecd3f473c8868e0e5ed13c744d5
Craig J Perry <craigp84@gmail.com>
2015-04-18 23:01:32 +0100

    test(PresentationFormat): Add cucumber boiler plate

---
+++ canbusexplorer-integration-tests/src/test/java/com/craigjperry/canbusexplorer/feature/presentationformat/PresentationFormatSteps.java
@@ -0,0 +1,26 @@
+package com.craigjperry.canbusexplorer.feature.presentationformat;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class PresentationFormatSteps {
+    @Given("^the app has been started$")
+    public void the_app_has_been_started() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I visit the application URL on my (\\w+)$")
+    public void i_visit_the_application_URL_on_my(String deviceType) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see the panel (\\w+)$")
+    public void i_should_see_the_panel(String panelName) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
---
+++ canbusexplorer-integration-tests/src/test/java/com/craigjperry/canbusexplorer/feature/presentationformat/PresentationFormatTest.java
@@ -0,0 +1,8 @@
+package com.craigjperry.canbusexplorer.feature.presentationformat;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class PresentationFormatTest {
+}
