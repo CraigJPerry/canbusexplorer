f2d0be55bee10ee4c39cdd277f171aca1ef17f10
Craig J Perry <craigp84@gmail.com>
2015-04-19 22:47:01 +0100

    test(PresentationFormat): Improve consistency with other tests by adopting AssertJ here too
     Also improves readability, thanks to the fluentlenium assertj assertions.

--- canbusexplorer-integration-tests/src/test/java/com/craigjperry/canbusexplorer/feature/presentationformat/PresentationFormatSteps.java
+++ canbusexplorer-integration-tests/src/test/java/com/craigjperry/canbusexplorer/feature/presentationformat/PresentationFormatSteps.java
@@ -1,46 +1,43 @@
 package com.craigjperry.canbusexplorer.feature.presentationformat;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.fluentlenium.core.FluentAdapter;
+import org.fluentlenium.core.annotation.Page;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 
-public class PresentationFormatSteps {
+public class PresentationFormatSteps extends FluentAdapter {
 
-    public WebDriver driver;
+    @Page
+    CanbusExplorerSinglePageApplication appPage;
 
     @Before
-    public void setup() {
-        driver = new FirefoxDriver();
+    public void before() {
+        this.initFluent(new HtmlUnitDriver());
+        this.initTest();
     }
 
     @After
-    public void closeBrowser() {
-        driver.quit();
+    public void after() {
+        this.quit();
     }
 
     @Given("^the app has been started$")
     public void the_app_has_been_started() throws Throwable {
-        driver.get("http://localhost:8080");
-        assertThat(driver.getTitle(), equalTo("Canbus Explorer"));
+        appPage.go();
     }
 
     @When("^I visit the application URL on my (\\w+)$")
     public void i_visit_the_application_URL_on_my(String deviceType) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        appPage.isAt();
     }
 
     @Then("^I should see the panel (\\w+)$")
     public void i_should_see_the_panel(String panelName) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        appPage.isVisible(panelName);
     }
 }
