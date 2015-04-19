155212c1a7e223a7334d3c3a342bfc3ca4f3a08b
Craig J Perry <craigp84@gmail.com>
2015-04-19 01:05:05 +0100

    test(PresentationFormat): Selenium firefox visits test server url from Cucumber

--- canbusexplorer-integration-tests/src/test/java/com/craigjperry/canbusexplorer/feature/presentationformat/PresentationFormatSteps.java
+++ canbusexplorer-integration-tests/src/test/java/com/craigjperry/canbusexplorer/feature/presentationformat/PresentationFormatSteps.java
@@ -1,15 +1,35 @@
 package com.craigjperry.canbusexplorer.feature.presentationformat;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PresentationFormatSteps {
+
+    public WebDriver driver;
+
+    @Before
+    public void setup() {
+        driver = new FirefoxDriver();
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
+
     @Given("^the app has been started$")
     public void the_app_has_been_started() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        driver.get("http://localhost:8080");
+        assertThat(driver.getTitle(), equalTo("Canbus Explorer"));
     }
 
     @When("^I visit the application URL on my (\\w+)$")
