68e28aa09e3a2e24a8cb3522b5482c87d90d1edd
Craig J Perry <craigp84@gmail.com>
2015-05-10 00:12:10 +0100

    chore(pom): integration tests exclude cases which require live dongles

--- canbusexplorer-integration-tests/src/test/java/com/craigjperry/canbusexplorer/feature/presentationformat/PresentationFormatTest.java
+++ canbusexplorer-integration-tests/src/test/java/com/craigjperry/canbusexplorer/feature/presentationformat/PresentationFormatTest.java
@@ -1,11 +1,8 @@
 package com.craigjperry.canbusexplorer.feature.presentationformat;
 
-import com.craigjperry.canbusexplorer.testsupport.categories.UITest;
 import cucumber.api.junit.Cucumber;
-import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-@Category(UITest.class)
 @RunWith(Cucumber.class)
 public class PresentationFormatTest {
 }
--- canbusexplorer-integration-tests/src/test/java/com/craigjperry/canbusexplorer/testsupport/categories/UITest.java
+++
@@ -1,8 +0,0 @@
-package com.craigjperry.canbusexplorer.testsupport.categories;
-
-/**
- * UITest category of JUnit tests. Categories are selected (or excluded)
- * by the maven failsafe groups configuration.
- */
-public interface UITest {
-}
